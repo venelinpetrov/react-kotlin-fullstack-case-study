cd121d2d1b714d6e15cc5065587b9406599af3ff
venelinpetrov <vpetrov33tu@gmail.com>
2025-06-09 17:45:36 +0300

    Basic fetch. Basic types

---
+++ backend/src/main/kotlin/com/example/todo_app/config/WebConfig.kt
@@ -0,0 +1,22 @@
+package com.example.todo_app.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class WebConfig {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:5173")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            }
+        }
+    }
+}
